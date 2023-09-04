fbe8e197014115b2c10d5155a1e74fabf1492e5f
Aras <vahitaras@gmail.com>
2023-09-04 14:49:53 -0400

    finished working on dropdown list of username login page

--- Controller/controllers/shopController.js
+++ Controller/controllers/shopController.js
@@ -97,6 +97,9 @@ exports.getProduct = async (req, res, next) => {
 exports.getAllUsers = async (req, res, next) => {
   const allUsers = await dbAdminOperation.getAllUsers();
 
+  console.log(typeof res.locals.selectedUser.userId);
+  console.log(typeof allUsers[0]._id);
+
   res.render("shop/login", {
     pagePath: "/login",
     renderTitle: "Login",
